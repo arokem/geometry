ac330ac9a398473c5ff061504d66fdc42047f4de
Ariel Rokem <arokem@gmail.com>
2020-07-28 17:21:54 +0000

    Add doctest

--- geometry/circle.py
+++ geometry/circle.py
@@ -1,6 +1,24 @@
 import numpy as np
 
 def calculate_area(radius):
+    
+    """ 
+    Calculates the area of a circle
+    
+    Parameters
+    ----------
+    radius : number or array
+        The radius of a circle or many circles
+    
+    Returns 
+    -------
+    area : the calculate area/s
+    
+    Examples
+    --------
+    >>> calculate_area(1)
+    3.141592653589793
+    """
     area = np.pi * radius ** 2
     return area
 
